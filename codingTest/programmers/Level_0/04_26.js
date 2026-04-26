425eddeadad219df963e7b77c01c3dd59e76e453
biyuns <164608385+biyuns@users.noreply.github.com>
2026-04-26 21:50:49 +0900

    04_26 codingTest code

---
+++ codingTest/programmers/Level_0/04_26.js
@@ -0,0 +1,13 @@
+// 머쓱이네 피자가게는 피자를 일곱 조각으로 잘라 줍니다. 피자를 나눠먹을 사람의 수 n이 주어질 때, 모든 사람이 피자를 한 조각 이상 먹기 위해 필요한 피자의 수를 return 하는 solution 함수를 완성해보세요.
+const solution = n => Math.ceil(n/7);
+
+// 머쓱이네 피자가게는 피자를 두 조각에서 열 조각까지 원하는 조각 수로 잘라줍니다. 피자 조각 수 slice와 피자를 먹는 사람의 수 n이 매개변수로 주어질 때, n명의 사람이 최소 한 조각 이상 피자를 먹으려면 최소 몇 판의 피자를 시켜야 하는지를 return 하도록 solution 함수를 완성해보세요.
+const solution = (slice, n) => Math.ceil(n/slice);
+
+// 머쓱이는 추운 날에도 아이스 아메리카노만 마십니다. 아이스 아메리카노는 한잔에 5,500원입니다. 머쓱이가 가지고 있는 돈 money가 매개변수로 주어질 때, 머쓱이가 최대로 마실 수 있는 아메리카노의 잔 수와 남는 돈을 순서대로 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+const solution = money => {
+    var answer = [];
+    answer.push(Math.floor(money / 5500));
+    answer.push(money % 5500)
+    return answer;
+}
