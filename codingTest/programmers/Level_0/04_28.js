da0f3a093b27e54c8efc2d8af3891db3a2637d5a
biyuns <164608385+biyuns@users.noreply.github.com>
2026-04-28 23:18:41 +0900

    0428 codingTest code

--- codingTest/programmers/Level_0/04_28.js
+++ codingTest/programmers/Level_0/04_28.js
@@ -1,3 +1,11 @@
-문자열 my_string과 문자 letter이 매개변수로 주어집니다. my_string에서 letter를 제거한 문자열을 return하도록 solution 함수를 완성해주세요.
+// 문자열 my_string과 문자 letter이 매개변수로 주어집니다. my_string에서 letter를 제거한 문자열을 return하도록 solution 함수를 완성해주세요.
 
 const solution = (my_string, letter) => my_string.split("").filter((item) => item !==letter).join("");
+
+// 정수가 담긴 리스트 num_list가 주어질 때, 리스트의 길이가 11 이상이면 리스트에 있는 모든 원소의 합을 10 이하이면 모든 원소의 곱을 return하도록 solution 함수를 완성해주세요.
+const solution = num_list => 
+    num_list.length <= 10 
+    ?
+    num_list.reduce((a, b) => a*b)
+    :
+    num_list.reduce((a, b) => a+b)
