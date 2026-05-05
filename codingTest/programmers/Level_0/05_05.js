ac8a273be99e6bcbc3ad38c172e61f052bb508ae
biyuns <164608385+biyuns@users.noreply.github.com>
2026-05-05 23:56:17 +0900

    0505 codingTest code

--- codingTest/programmers/Level_0/05_05.js
+++ codingTest/programmers/Level_0/05_05.js
@@ -5,3 +5,7 @@ const solution = n_str => Number(n_str);
 // 한 자리 정수로 이루어진 문자열 num_str이 주어질 때, 각 자리수의 합을 return하도록 solution 함수를 완성해주세요.
 
 const solution = num_str => num_str.split('').reduce((sum,item) => sum + Number(item), 0)
+
+// 알파벳으로 이루어진 문자열 myString이 주어집니다. 모든 알파벳을 소문자로 변환하여 return 하는 solution 함수를 완성해 주세요.
+
+const solution = myString => myString.toLowerCase();
