c935cc2edcf523ac1f8ff98e72684920929e492f
biyuns <164608385+biyuns@users.noreply.github.com>
2026-05-19 23:52:33 +0900

    0519 codingTest code

---
+++ codingTest/programmers/Level_0/05_19.js
@@ -0,0 +1,17 @@
+// 'm'과 "rn"이 모양이 비슷하게 생긴 점을 활용해 문자열에 장난을 하려고 합니다. 문자열 rny_string이 주어질 때, rny_string의 모든 'm'을 "rn"으로 바꾼 문자열을 return 하는 solution 함수를 작성해 주세요.
+
+const solution = rny_string => rny_string.replaceAll("m", "rn");
+
+// 단어가 공백 한 개로 구분되어 있는 문자열 my_string이 매개변수로 주어질 때, my_string에 나온 단어를 앞에서부터 순서대로 담은 문자열 배열을 return 하는 solution 함수를 작성해 주세요.
+const solution = my_string => my_string.split(" ");
+
+// 정수 리스트 num_list가 주어집니다. 가장 첫 번째 원소를 1번 원소라고 할 때, 홀수 번째 원소들의 합과 짝수 번째 원소들의 합 중 큰 값을 return 하도록 solution 함수를 완성해주세요. 두 값이 같을 경우 그 값을 return합니다.
+function solution(num_list) {
+    let odd = 0;
+    let even = 0; 
+    for(let i =0; i<num_list.length; i++) {
+        if(i % 2 === 0) odd += num_list[i]
+        else even += num_list[i]
+    }
+    return odd > even ? odd : even
+}
