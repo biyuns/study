dc18e5675d7ff3dfc68f763351d6cb250b4484f7
biyuns <164608385+biyuns@users.noreply.github.com>
2026-05-20 22:51:38 +0900

    0520 codingTest code

--- codingTest/programmers/Level_0/05_20.js
+++ codingTest/programmers/Level_0/05_20.js
@@ -1,2 +1,6 @@
 // 영소문자로 이루어진 문자열 my_string과 영소문자 1글자로 이루어진 문자열 alp가 매개변수로 주어질 때, my_string에서 alp에 해당하는 모든 글자를 대문자로 바꾼 문자열을 return 하는 solution 함수를 작성해 주세요.
 const solution = (my_string, alp) =>  my_string.replaceAll(alp, alp.toUpperCase());
+
+// 정수 배열 numbers와 정수 n이 매개변수로 주어집니다. numbers의 원소를 앞에서부터 하나씩 더하다가 그 합이 n보다 커지는 순간 이때까지 더했던 원소들의 합을 return 하는 solution 함수를 작성해 주세요.
+const solution = (numbers, n) => numbers.reduce((acc, cur) => acc <= n ? acc+cur : acc);
+
