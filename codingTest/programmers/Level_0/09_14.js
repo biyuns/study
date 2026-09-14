2c1138fb97dcdb98545e980c60d8d78ec3938b94
biyuns <164608385+biyuns@users.noreply.github.com>
2026-09-14 23:35:14 +0900

    0914 codingTest code
    프로그래머스 조건에 맞게 수열 변환하기 1

---
+++ codingTest/programmers/Level_0/09_14.js
@@ -0,0 +1,19 @@
+/*
+정수 배열 arr가 주어집니다. arr의 각 원소에 대해 값이 50보다 크거나 같은 짝수라면 2로 나누고, 50보다 작은 홀수라면 2를 곱합니다. 그 결과인 정수 배열을 return 하는 solution 함수를 완성해 주세요.
+
+제한사항
+1 ≤ arr의 길이 ≤ 1,000,000
+1 ≤ arr의 원소의 값 ≤ 100
+*/
+
+const solution = arr => {
+    return arr.map(item => {
+        if (item % 2 === 0 && item >= 50) {
+            return item / 2;
+        } else if (item % 2 === 1 && item < 50) {
+            return item * 2;
+        } else {
+            return item;
+        }
+    });
+}
