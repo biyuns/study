cef5d9b04faa021c5e16ccadc66b7bb9694bd128
biyuns <164608385+biyuns@users.noreply.github.com>
2026-09-21 23:51:14 +0900

    0921 codingTest code

---
+++ codingTest/programmers/Level_0/09_21.js
@@ -0,0 +1,20 @@
+/*
+정수 배열 arr과 정수 n이 매개변수로 주어집니다. arr의 길이가 홀수라면 arr의 모든 짝수 인덱스 위치에 n을 더한 배열을, arr의 길이가 짝수라면 arr의 모든 홀수 인덱스 위치에 n을 더한 배열을 return 하는 solution 함수를 작성해 주세요.
+
+제한사항
+1 ≤ arr의 길이 ≤ 1,000
+1 ≤ arr의 원소 ≤ 1,000
+1 ≤ n ≤ 1,000
+*/
+
+const solution = (arr, n) => {
+    if (arr.length % 2 === 0) {
+        return arr.map((item, index) =>
+            index % 2 === 1 ? item + n : item
+        );
+    } else {
+        return arr.map((item, index) =>
+            index % 2 === 0 ? item + n : item
+        );
+    }
+}
